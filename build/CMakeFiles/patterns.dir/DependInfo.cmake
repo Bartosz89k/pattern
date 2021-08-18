ab9c7d3ae4ae3a788ebf470801fdffe349d3ec6e
Bartosz Kopczyński <b.kopczynski@satrevolution.com>
2021-08-18 10:42:26 +0200

    init

---
+++ build/CMakeFiles/patterns.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/media/bk/3f78fad0-f1f5-4783-94ab-8e6ff6e2c664/bk3/src/cpp/github/cmakeQuickStart/main.cpp" "/media/bk/3f78fad0-f1f5-4783-94ab-8e6ff6e2c664/bk3/src/cpp/github/cmakeQuickStart/build/CMakeFiles/patterns.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /media/bk/3f78fad0-f1f5-4783-94ab-8e6ff6e2c664/bk3/src/cpp/github/cmakeQuickStart
+# Build directory: /media/bk/3f78fad0-f1f5-4783-94ab-8e6ff6e2c664/bk3/src/cpp/github/cmakeQuickStart/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.

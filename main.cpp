55fb0627f1a124d0373b44a78f72f716cd78ffb7
Bartosz89k <bartosz89.k@gmail.com>
2021-08-19 11:18:12 +0200

    Inheritance

--- main.cpp
+++ main.cpp
@@ -1,10 +1,39 @@
+//git remote add origin git@github.com:Bartosz89k/pattern.git
+
 #include <iostream>
 
-int main(int, char**) {
-    std::cout << "Hello, world! df \n";
+using namespace std;
 
-    //git remote add origin git@github.com:Bartosz89k/pattern.git
-    //git branch -M main
-    //git push -u origin main
+// factory design pattern
+
+class Machine {
+public:
+    ~Machine() { 
+        cout << "destructing Machine" << endl;       
+    }
+    void show(void) {
+        cout << "hiho mech" << endl;
+    }
+};
+
+class Robot: public Machine{
+    public:
     
+    void show(void) {
+        cout << "hiho rob" << endl;
+    }
+
+    ~Robot() {
+        cout << "destructing Robot" << endl;
+    }
+};
+
+int main(int, char**) {
+    std::cout << "Hello, world! df4 \n";
+
+    Machine * mech = new Robot();
+
+    mech->show();
+
+    return 0;    
 }

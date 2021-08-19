13c5e35d7103ab1f47bdbe0ccb811621864aebc6
Bartosz89k <bartosz89.k@gmail.com>
2021-08-19 09:57:44 +0200

    kk

--- main.cpp
+++ main.cpp
@@ -3,8 +3,8 @@
 int main(int, char**) {
     std::cout << "Hello, world! df \n";
 
-    // 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
-    // git config --list
-    // test2ed3
+    //git remote add origin git@github.com:Bartosz89k/pattern.git
+    //git branch -M main
+    //git push -u origin main
     
 }

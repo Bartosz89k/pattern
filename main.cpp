9c231ce3b35f4ee757f1880fce8a58b16a46da7e
Bartosz89k <bartosz89.k@gmail.com>
2021-08-19 16:09:47 +0200

    Factory pattern

--- main.cpp
+++ main.cpp
@@ -5,35 +5,84 @@
 using namespace std;
 
 // factory design pattern
+static int statI = 0;
 
 class Machine {
 public:
-    ~Machine() { 
-        cout << "destructing Machine" << endl;       
+
+    Machine () {
+        cout << "constructing Machine" << statI << endl;  
     }
-    void show(void) {
-        cout << "hiho mech" << endl;
+
+    virtual ~Machine() { 
+        cout << "destructing Machine" << statI << endl;       
+    }
+
+    virtual void show(void) {
+        cout << "hiho mech: " << statI <<  endl;
     }
 };
 
-class Robot: public Machine{
+class Axe: public Machine{
     public:
+
+    Axe () {
+        cout << "constructing Axe" << statI << endl;  
+    }
     
     void show(void) {
-        cout << "hiho rob" << endl;
+        cout << "hiho axe: " << statI << endl;
     }
 
-    ~Robot() {
-        cout << "destructing Robot" << endl;
+    ~Axe() {
+        cout << "destructing Axe" << statI << endl;
     }
+};
+
+class MachineFactory {
+    public:
+    
+    MachineFactory() {
+        cout << "constructor MachineFactory" << endl;
+    }
+
+    static Machine* createAxe() {
+        return new Axe();
+    }
+
+    ~MachineFactory() {
+        cout << "destructor MachineFactory" << endl;
+    }
+
+    // static Machine* createHummer() { return new Hummer(); }
 };
 
 int main(int, char**) {
+    
     std::cout << "Hello, world! df4 \n";
+    
+    {
+        Machine * mech = new Axe();
 
-    Machine * mech = new Robot();
+        mech->show();
+        delete mech;
 
-    mech->show();
+        Machine mech2;
+        mech2.show();
+
+        Axe axe;
+        axe.show();
+    }
+
+
+    std::cout << "last part \n" << endl;
+    {
+        Machine* axe = MachineFactory::createAxe();
+        axe->show();
+        delete axe;
+    }
+
+    std::cout << "end \n";
 
     return 0;    
 }
